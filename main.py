c9f53e24dd1a9edd545ac67b90e29948999b03ad
BilalAhmad096 <bilal.ahmad096@gmail.com>
2024-07-11 05:27:42 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding:  utf-8 -*-
+"""
+created on Thu July 11 05:22 2024
+
+@author: https://github.com/BilalAhmad096
+
+@project: Handwritten Text Extraction
+"""
+
+from src.handwritten_text_extraction import handwritten_text_extraction
+
+if __name__ == "__main__":
+    handwritten_text_extraction()
